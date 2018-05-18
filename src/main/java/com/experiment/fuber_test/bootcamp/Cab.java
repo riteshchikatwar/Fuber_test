a31ab4540c08b2aaf4f4f457ae2263ac18f89130
riteshchikatwar <ritesh.chikatwar@egovernments.org>
2018-05-18 16:37:30 +0530

    Test Commit

--- src/main/java/com/experiment/fuber_test/bootcamp/Cab.java
+++ src/main/java/com/experiment/fuber_test/bootcamp/Cab.java
@@ -20,26 +20,9 @@ public class Cab {
     
     private String email;
     
-    private Double longitude; 
     
-    private Double latitude;
 
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
+    
     public Long getId() {
         return id;
     }
